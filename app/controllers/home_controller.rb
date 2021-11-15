9ab612e8c9019646d0846c66bd2398c8c686fbd4
Anibal Montero <cubanibal@hotmail.com>
2021-11-15 18:10:51 +0100

    rectificadas rutas

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
 	def trash_em
 		@trash_em = Tarea.where(:completada => true).destroy_all
-		redirect_to tareas_url, notice: "Las tareas completadas han sido borradas exitosamente."
+		redirect_to root_url, notice: "Las tareas completadas han sido borradas exitosamente."
 	end
 	def trash_em_all
 		@trash_em_all = Tarea.all.destroy_all
-		redirect_to tareas_url, notice: "Todas las tareas han sido borradas exitosamente."
+		redirect_to root_url, notice: "Todas las tareas han sido borradas exitosamente."
 	end
 end
--- app/controllers/tareas_controller.rb
+++ app/controllers/tareas_controller.rb
@@ -26,7 +26,7 @@ class TareasController < ApplicationController
 
     respond_to do |format|
       if @tarea.save
-        format.html { redirect_to tareas_url, notice: "Tarea creada exitosamente." }
+        format.html { redirect_to root_url, notice: "Tarea creada exitosamente." }
         format.json { render :show, status: :created, location: @tarea }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -39,7 +39,7 @@ class TareasController < ApplicationController
   def update
     respond_to do |format|
       if @tarea.update(tarea_params)
-        format.html { redirect_to tareas_url, notice: "Tarea actualizada exitosamente." }
+        format.html { redirect_to root_url, notice: "Tarea actualizada exitosamente." }
         format.json { render :show, status: :ok, location: @tarea }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class TareasController < ApplicationController
   def destroy
     @tarea.destroy
     respond_to do |format|
-      format.html { redirect_to tareas_url, notice: "Tarea eliminada exitosamente." }
+      format.html { redirect_to root_url, notice: "Tarea eliminada exitosamente." }
       format.json { head :no_content }
     end
   end
